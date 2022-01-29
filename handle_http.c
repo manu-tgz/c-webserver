057fd071b45c48bf81f8326997fd5b593b0af64b
manu-tg <emmanueltorrestdad@gmail.com>
2022-01-29 12:10:15 -0500

    handle_http ya recive

--- handle_http.c
+++ handle_http.c
@@ -1,6 +1,26 @@
-void handle_http()
+#define BUFFER_SIZE 200
+void handle_http(int socket)
 {
-    
+    if (recv_request(socket) < 0)
+	{
+		perror("Receive");
+		exit(-1);
+	}
 }
 
-//TODO:imprimir peticiones de navegadores
+int  recv_request(int socket)
+{
+	char buffer[BUFFER_SIZE];
+	memset (buffer,'\0', BUFFER_SIZE);
+
+	if (recv(socket, buffer, BUFFER_SIZE, 0) == -1)
+	{
+		perror("Request");
+		return -1;
+	}
+
+    //TODO:imprimir peticiones de navegadores
+   
+return 1;
+}
+
